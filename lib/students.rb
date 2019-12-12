8e5d682739b8a6e32c6b03154f3ff504dca70e22
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-12-12 06:48:49 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,17 +1,21 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  "SELECT * FROM students ORDER BY gpa DESC LIMIT 1; "
+  "SELECT MAX(gpa) FROM students;"
 end
 
 def lowest_student_gpa
+  "SELECT MIN(students.gpa) FROM students;"
 end
 
 def average_student_gpa
+  "SELECT AVG(students.gpa) FROM students;"
 end
 
 def total_tardies_for_all_students
+  "SELECT SUM(students.tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  
 end
